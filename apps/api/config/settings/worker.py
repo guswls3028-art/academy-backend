eccdba62ecc122ffed4ae66a51dddb3d241639f3
hyunjin <guswls3028@naver.com>
2025-12-23 07:56:42 +0900

    update

--- apps/api/config/settings/worker.py
+++ apps/api/config/settings/worker.py
@@ -1,12 +1,13 @@
+# apps/api/config/settings/worker.py
+
 INSTALLED_APPS = [
     # Django 최소
     "django.contrib.contenttypes",
     "django.contrib.auth",
 
-    # Celery task가 실제로 쓰는 것들만
-    "apps.support.media",
+    # 실제 존재하는 것만
     "apps.worker",
 
-    # 필요 시
+    # Celery 결과 백엔드 쓰면만
     "django_celery_results",
 ]
