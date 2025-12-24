b72b28ebae8a07657975dc1589d7ae4467b63020
hyunjin <guswls3028@naver.com>
2025-12-24 23:34:29 +0900

    update

---
+++ apps/shared/__init__.py
@@ -0,0 +1,2 @@
+# apps/shared/__init__.py
+default_app_config = "apps.shared.apps.SharedConfig"
---
+++ apps/shared/apps.py
@@ -0,0 +1,5 @@
+# apps/shared/apps.py
+from django.apps import AppConfig
+
+class SharedConfig(AppConfig):
+    name = "apps.shared"
