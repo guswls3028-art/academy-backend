617c84f6c942757e4bc2f73f5d83926fe919191c
hyunjin <guswls3028@naver.com>
2025-12-26 13:27:13 +0900

    update

--- apps/worker/celery.py
+++ apps/worker/celery.py
@@ -1,34 +1,25 @@
 # apps/worker/celery.py
-
 print("🔥 WORKER CELERY LOADED 🔥")
 
 import os
-import django               # ✅ 추가
+import django
 from celery import Celery
 
-# Worker 전용 settings
 os.environ.setdefault(
     "DJANGO_SETTINGS_MODULE",
     "apps.api.config.settings.worker",
 )
 
-django.setup()               # ✅ 핵심 (이게 없어서 다 터졌음)
+django.setup()
 
-# 프로젝트 이름
 app = Celery("academy")
 
-# Django settings 로드
+# ✅ Django settings만 신뢰
 app.config_from_object(
     "django.conf:settings",
     namespace="CELERY",
 )
 
-# 🔥 추가 (이 줄이 핵심)
-app.conf.broker_url = os.environ.get("CELERY_BROKER_URL")
-app.conf.result_backend = os.environ.get("CELERY_RESULT_BACKEND")
-
-
-# ✅ 핵심: 인자 없이 autodiscover
 app.autodiscover_tasks()
 
 print("🔥 autodiscover_tasks called 🔥")
