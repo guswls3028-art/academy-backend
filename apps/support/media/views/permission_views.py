55d34050487734f5d8deb3b84b2a6672e5391ef3
hyunjin <guswls3028@naver.com>
2025-12-28 20:49:20 +0900

    update

--- apps/support/media/views/permission_views.py
+++ apps/support/media/views/permission_views.py
@@ -4,6 +4,10 @@
 
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import status
+
 from django_filters.rest_framework import DjangoFilterBackend
 
 from ..models import VideoPermission
@@ -16,3 +20,37 @@ class VideoPermissionViewSet(ModelViewSet):
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ["video", "enrollment"]
     permission_classes = [IsAuthenticated]
+
+    @action(detail=False, methods=["post"], url_path="bulk-set")
+    def bulk_set(self, request):
+        """
+        payload:
+        {
+          "video": 1,
+          "enrollments": [3,4,5],
+          "rule": "blocked"
+        }
+        """
+        video_id = request.data.get("video")
+        enrollments = request.data.get("enrollments", [])
+        rule = request.data.get("rule")
+
+        if not video_id or not enrollments or not rule:
+            return Response(
+                {"detail": "video, enrollments, rule required"},
+                status=400,
+            )
+
+        objs = []
+        for enrollment_id in enrollments:
+            obj, _ = VideoPermission.objects.update_or_create(
+                video_id=video_id,
+                enrollment_id=enrollment_id,
+                defaults={"rule": rule},
+            )
+            objs.append(obj)
+
+        return Response(
+            VideoPermissionSerializer(objs, many=True).data,
+            status=status.HTTP_200_OK,
+        )
