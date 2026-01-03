1fb80c7cfb7888360a61f9ee587a9077804cc87e
hyunjin <guswls3028@naver.com>
2026-01-04 06:28:17 +0900

    update

--- apps/shared/tasks/__init__.py
+++ apps/shared/tasks/__init__.py
@@ -1,11 +1,9 @@
 # apps/shared/tasks/__init__.py
 
 from .media import process_video_media
-from .ai import process_ai_submission_task
 from .ai_worker import run_ai_job_task
 
 __all__ = [
     "process_video_media",
-    "process_ai_submission_task",
     "run_ai_job_task",
 ]
