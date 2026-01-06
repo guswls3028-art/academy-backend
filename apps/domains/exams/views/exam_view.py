0455f48761c51fa1abd6dc4ea22751003eb72172
hyunjin <guswls3028@naver.com>
2026-01-07 05:35:10 +0900

    update

--- apps/domains/exams/views/exam_view.py
+++ apps/domains/exams/views/exam_view.py
@@ -4,6 +4,8 @@ from __future__ import annotations
 
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import ValidationError
+
 
 from apps.domains.exams.models import Exam
 from apps.domains.exams.serializers.exam import ExamSerializer
