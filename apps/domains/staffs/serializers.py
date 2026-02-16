843a2214d9b8df5650f5e811bc939e0b73d5442d
hyunjin <guswls3028@naver.com>
2026-02-17 08:19:23 +0900

    sync local with repo (auto)

--- apps/domains/staffs/serializers.py
+++ apps/domains/staffs/serializers.py
@@ -1,4 +1,6 @@
 # PATH: apps/domains/staffs/serializers.py
+import logging
+
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from django.db import transaction, IntegrityError
