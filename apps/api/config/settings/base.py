eee0fde5244e2063b6e69f853b5221f18bcc9dd1
hyunjin <guswls3028@naver.com>
2026-02-17 10:05:01 +0900

    sync local with repo (auto)

--- apps/api/config/settings/base.py
+++ apps/api/config/settings/base.py
@@ -246,7 +246,7 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(days=30),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=60),
+    "REFRESH_TOKEN_LIFETIME": timedelta(hours=12),
     "AUTH_HEADER_TYPES": ("Bearer",),
 }
 
