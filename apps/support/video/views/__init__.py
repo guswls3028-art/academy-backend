22446e7fdaef7eff182d75e7fc667de67cc4948b
hyunjin <guswls3028@naver.com>
2026-02-18 20:01:00 +0900

    sync local with repo (auto)

--- apps/support/video/views/__init__.py
+++ apps/support/video/views/__init__.py
@@ -4,6 +4,6 @@ from .video_policy_impact import VideoPolicyImpactAPIView
 
 from .video_views import VideoViewSet
 from .permission_views import VideoPermissionViewSet
-from .progress_views import VideoProgressViewSet
+from .progress_views import VideoProgressViewSet, VideoProgressView
 from .internal_views import VideoProcessingCompleteView
 from .event_views import VideoPlaybackEventViewSet
