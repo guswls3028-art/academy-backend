ab6fce15f4eac09efd29c7e0c3f3c66ada81179e
hyunjin <guswls3028@naver.com>
2026-02-19 18:25:55 +0900

    sync local with repo (auto)

--- apps/domains/clinic/views.py
+++ apps/domains/clinic/views.py
@@ -5,6 +5,8 @@ from django.utils import timezone
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
 
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
