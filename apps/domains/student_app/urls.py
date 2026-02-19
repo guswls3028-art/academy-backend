790f36ed6dcac792f3f73b178164a45bb83ddd2c
hyunjin <guswls3028@naver.com>
2026-02-19 14:26:19 +0900

    sync local with repo (auto)

--- apps/domains/student_app/urls.py
+++ apps/domains/student_app/urls.py
@@ -38,8 +38,8 @@ urlpatterns = [
     path("results/me/exams/<int:exam_id>/items/", MyExamResultItemsView.as_view()),
 
     # ✅ Video (Student Consumer)
-    # 세션별 영상 목록
+    path("video/me/", StudentVideoMeView.as_view()),
+    path("video/public-session/", StudentPublicSessionView.as_view()),
     path("video/sessions/<int:session_id>/videos/", StudentSessionVideoListView.as_view()),
-    # 영상 재생 정보 (정책 포함)
     path("video/videos/<int:video_id>/playback/", StudentVideoPlaybackView.as_view()),
 ]
