8c9883ed9f1022abe9a17316bc1e6e7939050b00
hyunjin <guswls3028@naver.com>
2026-02-21 13:13:17 +0900

    sync local with repo (auto)

--- apps/support/video/views/internal_views.py
+++ apps/support/video/views/internal_views.py
@@ -3,6 +3,7 @@
 from __future__ import annotations
 
 from rest_framework.views import APIView
+from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework import status
