e914e32a6aa44cbce8b04ec1750eef271b2cf99f
hyunjin <guswls3028@naver.com>
2026-02-21 15:14:32 +0900

    sync local with repo (auto)

--- apps/worker/video_worker/sqs_main.py
+++ apps/worker/video_worker/sqs_main.py
@@ -374,8 +374,8 @@ def main() -> int:
                 except Exception as e:
                     logger.exception("JOB_PROCESSING_FAILED | job_id=%s | video_id=%s | error=%s", job_id, video_id, e)
                     job_fail_retry(job_id, str(e)[:2000])
-                    job_obj.refresh_from_db()
-                    if job_obj.attempt_count >= VIDEO_JOB_MAX_ATTEMPTS:
+                    job_after = job_get_by_id(job_id)
+                    if job_after and job_after.attempt_count >= VIDEO_JOB_MAX_ATTEMPTS:
                         job_mark_dead(job_id, error_code="MAX_ATTEMPTS", error_message=str(e)[:2000])
                         logger.warning("JOB_DEAD | job_id=%s | video_id=%s | attempt_count=%s", job_id, video_id, job_obj.attempt_count)
                     queue.change_message_visibility(receipt_handle, FAILED_TRANSIENT_BACKOFF_SECONDS)
