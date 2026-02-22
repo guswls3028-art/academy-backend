5f26a76b8ba8a987dbafd4c4bbf0844af69b094f
hyunjin <guswls3028@naver.com>
2026-02-22 21:57:53 +0900

    sync local with repo (auto)

--- scripts/build_and_push_ecr_remote.ps1
+++ scripts/build_and_push_ecr_remote.ps1
@@ -76,11 +76,8 @@ if ($NoCache) { $envParts += "NO_CACHE=1" }
 if ($SkipPrune) { $envParts += "DOCKER_SKIP_PRUNE=1" }
 $envLine = if ($envParts.Count -gt 0) { "export " + ($envParts -join " ") } else { "" }
 
-if ($GitRepoUrl) {
-    $repoLine = "cd /home/ec2-user/build && (test -d academy && cd academy && git fetch && git reset --hard origin/main && git pull) || (git clone '" + $GitRepoUrl + "' academy && cd academy)"
-} else {
-    $repoLine = "cd /home/ec2-user/build && test -d academy || (echo ERROR: no academy dir. Use -GitRepoUrl; exit 1) && cd academy && git fetch && git reset --hard origin/main && git pull"
-}
+# 깃 레포: 있으면 pull, 없으면 clone (기본값으로 레포 사용)
+$repoLine = "cd /home/ec2-user/build && (test -d academy && (cd academy && git fetch && git reset --hard origin/main && git pull)) || (git clone '" + $GitRepoUrl + "' academy && cd academy)"
 
 # 한 줄로 합쳐서 SSM commands 배열을 1개 요소만 쓰기 (JSON 이스케이프 단순화)
 $oneLine = "set -e; " + $repoLine + "; cd /home/ec2-user/build/academy; "
